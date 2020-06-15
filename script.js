fc5ca9a90db4ac0497e95ecc6dffbcb4db81f99b
levine01 <mugenilevine@gmail.com>
2020-06-15 03:43:01 +0300

    my changes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const container =document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener
+("click", () => {
+    container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener
+("click", () => {
+    container.classList.remove("change");
+});
+
 const colors=["#6495ed", "#7fffd4", "#ffa07a", "#f08080",
 "#afeeee"];
 
@@ -5,9 +17,9 @@ let i= 0;
 
 Array.from(document.querySelectorAll('.nav-link'))
 .forEach((item) => {
-    item.style.cssText = background-color; ${
+    item.style.cssText = `background-color: ${
     colors[i++]
-    };
+    }`; 
 });
 
 
